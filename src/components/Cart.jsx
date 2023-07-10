32f17f52649d082891147b6432e80de1da2f4e55
piyushguptag <piyushguptagg83@gmail.com>
2023-07-10 15:03:28 +0530

    Redux tutorial and for later use

---
+++ src/components/Cart.jsx
@@ -0,0 +1,70 @@
+import React from 'react'
+import {AiFillDelete} from 'react-icons/ai'
+import { useDispatch, useSelector } from 'react-redux';
+
+
+const Cart = () => {
+  const {cartItems, subTotal, shipping, tax, total} = useSelector(state => state.cart);
+const dispatch = useDispatch();
+
+  const incrementHandler = (id)=>{
+    dispatch({type : "addToCart",  payload: {id}});
+    dispatch({type:"calculatePrice"});
+  };
+  const decrementHandler = (id)=>{
+    dispatch({type : "decrement",  payload: id});
+    dispatch({type:"calculatePrice"});
+  };
+  const deleteHandler = (id)=>{
+    dispatch({type :"deleteFromCart", payload : id});
+    dispatch({type:"calculatePrice"});
+  };
+
+  return (
+    <div className='cart'>
+      <main>
+        
+        {
+          cartItems.length >0 ? (
+            cartItems.map(i=>(
+              <CartItem imgSrc= {i.imgSrc}
+                        name={i.name}
+                        price={i.price}
+                        qty={i.quantity} 
+                        id={i.id} 
+                        key={i.id}
+                        increment={incrementHandler}
+                        decrement={decrementHandler}
+                        deleteHandler={deleteHandler} />
+            ))
+          ) : <h1> No items yet</h1>
+
+        }
+      </main>
+      <aside>
+          <h2>Subtotal : ${subTotal}</h2>
+          <h2>Shipping : ${shipping}</h2>
+          <h2>Tax : ${tax}</h2>
+          <h2>Total : ${total}</h2>
+        </aside>
+    </div>
+  )
+}
+
+const CartItem = ({imgSrc,name,price,qty, decrement, increment,deleteHandler, id})=>(
+  <div className="cartItem">
+    <img src={imgSrc} alt="Item" />
+    <article>
+      <h3>{name}</h3>
+      <p>${price}</p>
+    </article>
+    <div>
+      <button onClick={()=>decrement(id)}>-</button>
+      <p>{qty}</p>
+      <button onClick={()=>increment(id)}>+</button>
+    </div>
+    <AiFillDelete onClick={()=>deleteHandler(id)} />
+  </div>  
+)
+
+export default Cart
